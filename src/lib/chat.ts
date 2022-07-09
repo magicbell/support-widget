8e3d471600f9ed7b1e4555e22a015b4db907d898
Stephan Meijer <stephan.meijer@gmail.com>
2022-07-09 16:34:18 +0200

    chore: improve chat flow

--- src/lib/chat.ts
+++ src/lib/chat.ts
@@ -9,13 +9,13 @@ export const chat: ChatFn = async (bot) => {
     message: '',
   };
 
-  data.message = await bot.ask('Write a message…');
-
   await bot.say(
-    'Hi! Thank you for your message. Can you answer a few questions for me, so we can help you?'
+    'Hi! Thanks for reaching out. Before we get started, can you please tell me your name?'
   );
-  await bot.say("Let's start with your name. How should we call you?");
-  data.name = await bot.ask('Please enter your name…');
+
+  data.name = await bot.ask('Please enter your name…', {
+    validate: async (value) => value.trim().length > 0,
+  });
 
   await bot.say(
     `Thanks, ${data.name}! At what email address can we reach you?`
@@ -31,6 +31,13 @@ export const chat: ChatFn = async (bot) => {
     },
   });
 
+  await bot.say(
+    `Great! Now please write your question. Once you hit enter, I\'ll create a ticket, and our support staff will reach out to you at ${data.email}.`
+  );
+  data.message = await bot.ask(`Please write your question…`, {
+    validate: async (value) => value.trim().length > 0,
+  });
+
   await bot.say("Awesome! I'll forward this to our support staff. One sec.");
 
   const { error, ticket } = await createTicket(data);
@@ -42,7 +49,8 @@ export const chat: ChatFn = async (bot) => {
     await bot.say(`Sorry, something went wrong!`);
     await bot.alert(error);
   } else {
-    await bot.say(`Done! Your ticket number is #${ticket.id}.`);
-    await bot.say(`Our human staff will contact you soon. Thanks again!`);
+    await bot.say(
+      `Done! Your ticket number is #${ticket.id}. Our human staff will contact you soon. Thanks again!`
+    );
   }
 };
